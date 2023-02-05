678b49a5c8c4037cd5603a0f289652b70171bbff
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-02-05 09:15:18 +0530

    Update script.js

--- siteTemplate/script.js
+++ siteTemplate/script.js
@@ -1,1 +1,34 @@
 
+const html = document.documentElement,
+    nav = document.querySelector("nav");
+
+function _(el) { return document.getElementById(el); }
+
+window.onscroll = () => {
+    if (html.scrollTop < 100) {
+        nav.classList.remove("scrolled");
+
+
+    } else {
+        nav.classList.add("scrolled");
+
+    }
+}
+
+function menuBtn(e) {
+    if (_("mobileNav").style.left != "0%") {
+        _('mobileNav').style.left = "0%";
+    } else {
+        _("mobileNav").style.left = "-100%";
+    }
+}
+
+function navBtnClick(e) {
+    // console.log(e.innerText)     //testing
+    _("mobileNav").style.left = "-100%";
+}
+
+function cancelNavBtn(e) {
+    _("mobileNav").style.left = "-100%";
+
+}
