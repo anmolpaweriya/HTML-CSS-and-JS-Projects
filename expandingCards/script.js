2a4d5534470bb0d6df4bbec38a85f2b2959e2d39
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-02-05 07:34:06 +0530

    Update script.js

--- expandingCards/script.js
+++ expandingCards/script.js
@@ -1,1 +1,10 @@
 
+// console.log("connected")
+
+Array.from(document.querySelectorAll(".cards")).forEach(e=>{
+    e.addEventListener("click",el=>{
+        document.querySelector(".active").classList.remove("active")
+        console.log("clicked");
+        e.classList.add("active")
+    })
+})
