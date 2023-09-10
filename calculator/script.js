8eb28abc7976c600a58cb77484b03eaef3638eb6
Anisole <64343922+anisolepro@users.noreply.github.com>
2023-09-10 10:41:15 +0530

    Update script.js

--- calculator/script.js
+++ calculator/script.js
@@ -1,122 +1,83 @@
 function _(el) { return document.querySelector(el); }
 
-previousData = 0;
-onHoldData = 0;
-currentOperation = "";
-nextValue = false;
+let calStr = "";
 
-function allClearFunc() {       // function to cleare all previous outputs
-    _("#display").innerText = "0";
-    previousData = 0;
-    onHoldData = 0;
-    currentOperation = "";
-    nextValue = false;
+
+function isSymbol(char) {
+
+    return '+-*/'.includes(char)
 }
 
-function numbPress(numb) {      // function for and number press 
-    if (_("#display").innerText == "0" || nextValue)
-        _("#display").innerText = "";
+function btnClick(e) {
 
-    nextValue = false;
-    _("#display").innerText += numb;
-    previousData = _("#display").innerText;
-}
-
-function dotPress() {       // function for dot btn
-    if (_("#display").innerText.includes("."))
-        return;
-
-    _("#display").innerText += ".";
-}
-
-function equalPress() { // function for equal to btn
-    if (currentOperation == "")
-        _("#display").innerText = previousData;
-    else if (currentOperation == "+")
-        _("#display").innerText = Number(onHoldData) + Number(previousData);
-
-    else if (currentOperation == "-")
-        _("#display").innerText = Number(onHoldData) - Number(previousData);
-
-    else if (currentOperation == "*")
-        _("#display").innerText = Number(onHoldData) * Number(previousData);
-
-    else if (currentOperation == "/")
-        _("#display").innerText = Number(onHoldData) / Number(previousData);
+    let appendNumb = e.innerText;
 
 
-    // reseting on hold data
 
-    if ("+-*/".includes(currentOperation)) {
-        currentOperation = "";
-        onHoldData = 0;
+
+    if (isSymbol(appendNumb) && calStr == "")
+        appendNumb = "0" + appendNumb;
+    else if (appendNumb == '.' && isSymbol(calStr.at(-1)))
+        appendNumb = '0.'
+
+
+
+
+
+    if (isSymbol(appendNumb) && isSymbol(calStr.at(-1))) {
+        calStr = calStr.slice(0, calStr.length - 1) + appendNumb;
+
     }
-    previousData = _("#display").innerText;
+    else
+        calStr += appendNumb;
 
-}
-
-function plusPress() {       // function for add btn
-    onHoldData = previousData;
-    currentOperation = "+";
-    nextValue = true;
+    console.log(calStr)
+    _('#display').innerText = calStr;
 }
 
-function minusPress() {       // function for add btn
-    onHoldData = previousData;
-    currentOperation = "-";
-    nextValue = true;
-}
-function multiplyPress() {       // function for add btn
-    onHoldData = previousData;
-    currentOperation = "*";
-    nextValue = true;
-}
-function dividePress() {       // function for add btn
-    onHoldData = previousData;
-    currentOperation = "/";
-    nextValue = true;
+
+function allClear() {
+    calStr = ""
+    _('#display').innerText = '0'
 }
 
-function exchangeSign() {       // function for add btn
-    previousData = -Number(_("#display").innerText);
-    _("#display").innerText = previousData;
-}
 
-function getSqrRoot() {       // function for add btn
-    previousData = Number(_("#display").innerText);
-    previousData = Math.sqrt(previousData).toFixed(8);
-    _("#display").innerText = previousData;
+function calculateResult() {
+
+    if (isSymbol(calStr.at(-1)))
+        calStr = calStr.slice(0, calStr.length - 1)
+
+    calStr = eval(calStr)
+    _('#display').innerText = calStr;
 
 }
 
 
+function squareRoot() {
 
-Array.from(document.querySelectorAll("td:not(#display)")).forEach(element => {
-    element.addEventListener('click', e => {
-        if (!isNaN(e.target.innerText))
-            numbPress(e.target.innerText)
-        else if (_("#display").innerText == "0" && currentOperation == "")
-            return
+    if (!isNaN(calStr)) {
 
-        if (e.target.innerText == "AC")
-            allClearFunc();
-        else if (e.target.innerText == ".")
-            dotPress();
-        else if (e.target.innerText == "+")
-            plusPress();
-        else if (e.target.innerText == "−")
-            minusPress();
-        else if (e.target.innerText == "×")
-            multiplyPress();
-        else if (e.target.innerText == "÷")
-            dividePress();
-        else if (e.target.innerText == "=")
-            equalPress();
-        else if (e.target.innerText == "+/-")
-            exchangeSign();
-        else if (e.target.innerText == "√")
-            getSqrRoot();
+        calStr = Math.sqrt(calStr);
+        _('#display').innerText = calStr;
 
-    })
-});
 
+    }
+}
+
+
+
+function reverseSign() {
+
+
+    if (!isNaN(calStr)) {
+        if (calStr[0] == '-')
+            calStr = calStr.slice(1)
+        else
+            calStr = '-' + calStr
+
+        _('#display').innerText = calStr;
+
+
+    }
+
+}
